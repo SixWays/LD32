91a4651de00259a3d107a9cd697f317b9762fcd5
Luke <luke.thompson@commonmode.co.uk>
2015-04-19 14:47:09 +0100

    AI infinite loop, maybe fixed now?

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -8,6 +8,9 @@ public class Monster : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		StartCoroutine(AILoop());
+		if (jumpy){
+			StartCoroutine(Moving(aiRate));
+		}
 		SphereCollider trig=null;
 		foreach (SphereCollider c in GetComponentsInChildren<SphereCollider>(true)){
 			if (c.isTrigger){
@@ -35,7 +38,20 @@ public class Monster : MonoBehaviour {
 	private float rotRate=1.5f;
 	[SerializeField]
 	private float speed=0.5f;
+	[SerializeField]
+	private bool jumpy=false;
+	[SerializeField]
+	private float pauseMin=0.5f;
+	[SerializeField]
+	private float pauseMax=2f;
+	[SerializeField]
+	private float moveMin=2f;
+	[SerializeField]
+	private float moveMax=4f;
+	bool paused=false;
+	private float aiRate=0.05f;
 
+	private bool tracking=false;
 	private bool vis=false;
 	IEnumerator AILoop(){
 		while (true){
@@ -44,34 +60,66 @@ public class Monster : MonoBehaviour {
 				if (Physics.Linecast(transform.position,PlayerCharacter.pos,out hit)){
 					if (PlayerCharacter.CheckCollider(hit.collider)){
 						vis=true;
+						tracking = true;
 						crumb = PlayerCharacter.pos;
 						crumb.y = transform.position.y;
-						Debug.DrawLine(transform.position,PlayerCharacter.pos,Color.green,0.05f);
+						//Debug.DrawLine(transform.position,PlayerCharacter.pos,Color.green,aiRate);
 					} else {
 						vis=false;
-						Debug.DrawLine(transform.position,PlayerCharacter.pos,Color.red,0.05f);
+						//Debug.DrawLine(transform.position,PlayerCharacter.pos,Color.red,aiRate);
 					}
 				} else {
 					vis=false;
 				}
 			}
-			yield return new WaitForSeconds(0.05f);
+			//yield return new WaitForSeconds(aiRate);
+		}
+	}
+	IEnumerator Pause(float dT){
+		paused=true;
+		float t = Random.Range(pauseMin,pauseMax);
+		if (active && tracking){
+			while (t>0){
+				/*
+				if (vis){
+					break;
+				}*/
+				t-=dT;
+				yield return new WaitForSeconds(dT);
+			}
+			StartCoroutine(Moving(dT));
+		}
+	}
+	IEnumerator Moving(float dT){
+		paused = false;
+		float t = Random.Range(moveMin,moveMax);
+		if (active && tracking){
+			while (t>0){
+				/*
+				if (vis){
+					break;
+				}*/
+				t-=dT;
+				yield return new WaitForSeconds(dT);
+			}
+			StartCoroutine(Pause(dT));
 		}
 	}
 	void FixedUpdate(){
-		if (active){
+		if (active && tracking){
 			// Track crumb
 			float angle = Vector3.Angle(transform.forward,(crumb-transform.position));
 			Vector3 target = (crumb-transform.position).normalized;
 			// Rate is per 180 degrees
 			transform.forward = Vector3.SmoothDamp(transform.forward,target,ref rRate, rotRate*angle/180);
 
-			// Check approach distance
-			if (vis){
-
-			}
-			if (Vector3.Distance(transform.position,crumb) > (vis?1.2f:0.5f)){
-				transform.position += transform.forward*speed*Time.fixedDeltaTime;
+			if (!paused){
+				// Check approach distance
+				if (Vector3.Distance(transform.position,crumb) > (vis?1.2f:0.5f)){
+					transform.position += transform.forward*speed*Time.fixedDeltaTime;
+				} else if (vis){
+					// Bounce!
+				}
 			}
 		}
 	}
